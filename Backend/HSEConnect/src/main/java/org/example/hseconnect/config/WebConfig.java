3fe7e5f5609a8f802b7839692a631275663e87c0
Darya Zhdanok <knemune@gmail.com>
2026-06-12 00:44:52 +0300

    Event card fix

--- Backend/HSEConnect/src/main/java/org/example/hseconnect/config/WebConfig.java
+++ Backend/HSEConnect/src/main/java/org/example/hseconnect/config/WebConfig.java
@@ -9,14 +9,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:63342",
                         "http://localhost:8080",
+                        "http://localhost:3000",
+                        "http://127.0.0.1:5500",
                         "https://hseconnect-wf7e.onrender.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
+                .exposedHeaders("*")
                 .allowCredentials(false);
     }
 }
